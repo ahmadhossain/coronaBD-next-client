364583807944018b9f6a35a2b15f631b944ad007
Ahmad Hossain <ahmadhossain.bu@gmail.com>
2021-12-18 19:29:45 +0600

    dynamic data

--- components/Card.jsx
+++ components/Card.jsx
@@ -12,22 +12,21 @@ function Card({users}) {
         <div className="flex-col divide-y-2 px-7 py-7 space-y-3 shadow-xl rounded-2xl bg-white">
             <div className="divide-y-2 pl-20 pr-5 space-y-3">
                 <div className="text-right">
-                    <div className="text-fuchsia-900 text-6xl md:text-7xl">{users}</div>
+                    <div className="text-fuchsia-900 text-6xl md:text-7xl">{users.todaycases}</div>
                     <div className="text-gray-600 text-lg md:text-xl">New Cases</div>
-                    {/* {console.log(posts)} */}
                 </div>
                 <div className="text-right">
-                    <div className="text-red-500 text-6xl md:text-7xl">{data.todayDeaths}</div>
+                    <div className="text-red-500 text-6xl md:text-7xl">{users.todayDeaths}</div>
                     <div className="text-gray-600 text-lg md:text-xl">New Deaths</div>
                 </div>
             </div>
             <div className="flex pt-3  divide-x-2">
                 <div className="text-right pr-2">
-                    <div className="text-fuchsia-900 text-2xl md:text-3xl">{data.cases}</div>
+                    <div className="text-fuchsia-900 text-2xl md:text-3xl">{users.cases}</div>
                     <div className="text-gray-600 text-sm md:text-base">Total Cases</div>
                 </div>
                 <div className="text-right pl-2">
-                    <div className="text-red-500 text-2xl md:text-3xl">{data.deaths} </div>
+                    <div className="text-red-500 text-2xl md:text-3xl">{users.deaths} </div>
                     <div className="text-gray-600 text-sm md:text-base">Total Deaths</div>
                 </div>
             </div>
